f907ea34f7b046ee3ee6d40dcb8d2ee781d1d326
Raphaël Benzazon <r.benzazon@webpublication.fr>
2019-10-16 20:15:55 +0300

    adds build folder for demo

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# joust 
+platform game inspired by Joust
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ build/precache-manifest.c5ee45f523cbf246eac174c3f63c737a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb6cc5da367f564ee6644b9b0dc05c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "599dd45540c92c66103d",
+    "url": "/static/css/main.54a598f1.chunk.css"
+  },
+  {
+    "revision": "06c3ac61a1aa08a824ba",
+    "url": "/static/js/2.23ad7461.chunk.js"
+  },
+  {
+    "revision": "599dd45540c92c66103d",
+    "url": "/static/js/main.31712e0a.chunk.js"
+  },
+  {
+    "revision": "a8b88b6d9183153896fb",
+    "url": "/static/js/runtime-main.a5694b11.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
